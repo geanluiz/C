b742ea34254598c89d17e8a6e2b6da904d281dc0
Gean Luiz S Ribeiro <97045487+geanluiz@users.noreply.github.com>
2024-08-09 18:51:33 +0000

    Made enum list more flexible and colorName function more succint

--- Exercism/04-enum-resistor/resistor_color.c
+++ Exercism/04-enum-resistor/resistor_color.c
@@ -10,44 +10,22 @@ int main (int argc, char *argv[])
 
     char *userInput = argv[1];
 
-	resistor_band_t color = colorName(userInput);
-
-	//const resistor_band_t *colors();
-
-    
+	resistor_band_t color = colorName(userInput);    
     printf("The color number is %i\n", color_code(color));
 }
 
 resistor_band_t colorName(char *name)
 {
-	if (strcmp(name, "BLACK") == 0)
-		return BLACK;
-	if (strcmp(name, "BROWN") == 0)
-		return BROWN;
-	if (strcmp(name, "RED") == 0)
-		return RED;
-	if (strcmp(name, "ORANGE") == 0)
-		return ORANGE;
-	if (strcmp(name, "YELLOW") == 0)
-		return YELLOW;
-	if (strcmp(name, "GREEN") == 0)
-		return GREEN;
-	if (strcmp(name, "BLUE") == 0)
-		return BLUE;
-	if (strcmp(name, "VIOLET") == 0)
-		return VIOLET;
-	if (strcmp(name, "GREY") == 0)
-		return GREY;
-	if (strcmp(name, "WHITE") == 0)
-		return WHITE;
-}
-
-const resistor_band_t p[10] = {
-	BLACK, BROWN, RED, ORANGE, YELLOW, GREEN, BLUE, VIOLET, GREY, WHITE};
+	for (int i = 0; i < sizeof(color_num); i++)
+	{
+		if (strcmp(name, color_name[i]) == 0)
+			return color_num[i];
+	}
+} 
 
 const resistor_band_t *colors() // Returns all colors to the test that checks if enum is correct
 {
-    return &p[0];
+    return &color_num[0];
 }
 
 resistor_band_t color_code(int color)
@@ -55,28 +33,3 @@ resistor_band_t color_code(int color)
     resistor_band_t col = color;
     return col;
 }
-
-/* #include <stdio.h>
-
-#define COLOR_TABLE \
-X(red, "red")       \
-X(green, "green")   \
-X(blue, "blue")
-
-#define X(a, b) a,
-enum COLOR {
-  COLOR_TABLE
-};
-#undef X
-
-#define X(a, b) b,
-char *color_name[] = {
-  COLOR_TABLE
-};
-#undef X
-
-int main() {
-  enum COLOR c = red;
-  printf("c=%s\n", color_name[c]);
-  return 0;
-} */
--- Exercism/04-enum-resistor/resistor_color.h
+++ Exercism/04-enum-resistor/resistor_color.h
@@ -6,18 +6,35 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define COLOR_TABLE \
+X(BLACK, "BLACK")   \
+X(BROWN, "BROWN")   \
+X(RED, "RED")       \
+X(ORANGE, "ORANGE") \
+X(YELLOW, "YELLOW") \
+X(GREEN, "GREEN")   \
+X(BLUE, "BLUE")     \
+X(VIOLET, "VIOLET") \
+X(GREY, "GREY")     \
+X(WHITE, "WHITE")
+
+#define X(a, b) a,
 typedef enum {
-    BLACK,
-    BROWN,
-    RED,
-    ORANGE,
-    YELLOW,
-    GREEN,
-    BLUE,
-    VIOLET,
-    GREY,
-    WHITE
+    COLOR_TABLE
 } resistor_band_t;
+#undef X
+
+#define X(a, b) a,
+resistor_band_t color_num[] = {
+  COLOR_TABLE
+};
+#undef X
+
+#define X(a, b) b,
+char *color_name[] = {
+  COLOR_TABLE
+};
+#undef X
 
 resistor_band_t colorName(char *name);
 
